629ff8dfa8e636836a754c3d5374269b68efe669
Aleraky <waelalerky@gmail.com>
2024-07-30 18:21:43 +0300

    adding converters

---
+++ GrocerrrrrryApp/Converters/StringToColorConverter.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Maui.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrocerrrrrryApp.Converters
+{
+    public class StringToColorConverter : BaseConverterOneWay<string, Color>
+    {
+        public override Color DefaultConvertReturnValue { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override Color ConvertFrom(string value, CultureInfo? culture) =>
+            Color.FromHex(value);
+    }
+}
