51932f95036c4daeb9f3bd0a2c2fd3fc3aae5a49
Aleraky <waelalerky@gmail.com>
2024-07-30 19:05:29 +0300

    adding enttiiews

---
+++ Api/Data/Entities/Address.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GrocerrrrrryApp.Api.Data.Entities
+{
+    [Table(nameof(Address))]
+    public class Address
+    {
+        [Key]
+        public long Id { get; set; }
+        [Required,MaxLength(15)]
+        public string Name { get; set; }
+        [Required,MaxLength(150)]
+        public string Address1 { get; set; }
+        [MaxLength(150)]
+        public string? Address2 { get; set; }
+        [Required, MaxLength(6)]
+        public string ZipCode { get; set; }
+        public bool IsPrimary { get; set; }
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+    }
+}
---
+++ Api/Data/Entities/OrderItem.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GrocerrrrrryApp.Api.Data.Entities
+{
+    [Table(nameof(OrderItem))]  
+    public class OrderItem
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required, MaxLength(100)]
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public decimal Amount { get; set; }
+        public decimal Quantity { get; set; }
+        public long OrderId { get; set; }
+        public virtual Order Order { get; set; }
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
