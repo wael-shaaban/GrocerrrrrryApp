dbb08dd44e27fc3b22e86c92279e5eabd1f73949
Aleraky <waelalerky@gmail.com>
2024-07-30 18:36:49 +0300

    adding handler

---
+++ GrocerrrrrryApp/Platforms/Android/AndroidHttpMessageHandler.cs
@@ -0,0 +1,25 @@
+﻿using GrocerrrrrryApp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Android.Net;
+
+namespace GrocerrrrrryApp.Platforms.Android
+{
+    public class AndroidHttpMessageHandler : IPlatformMessageHanger
+    {
+        public HttpMessageHandler GetHttpMessageHandler() =>
+        new AndroidMessageHandler
+        {
+            ServerCertificateCustomValidationCallback = (message, cert, chain, errors) =>
+            {
+                if (cert != null && cert.Issuer.Equals("CN=localhost"))
+                    return true;
+                return errors == System.Net.Security.SslPolicyErrors.None;
+            }
+        };
+    }
+}
---
+++ GrocerrrrrryApp/Platforms/iOS/IosHttpMessageHandler.cs
@@ -0,0 +1,18 @@
+﻿using GrocerrrrrryApp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrocerrrrrryApp.Platforms.iOS
+{
+    public class IosHttpMessageHandler : IPlatformMessageHanger
+    {
+        public HttpMessageHandler GetHttpMessageHandler() =>
+        new NSUrlSessionHandler
+        {
+            TrustOverrideForUrl = (sender, url, trust) => url.StartsWith("https://localhost")
+        };
+    }
+}
